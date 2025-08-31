b3f67581770622c406845ba48e80d6a9b015f70a
Yoiber dev <yoiber696@gmail.com>
2025-08-31 03:50:03 -0500

    fixed(TaskItem): Eliminando uso innecesario de useCallback

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -1,5 +1,5 @@
 import type { Task } from "../../Types/Task";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import TaskEditMode from "./TaskEditMode";
 import TaskViewMode from "./TaskViewMode";
 
@@ -14,11 +14,11 @@ const TaskItem = ({ task, onDeleteTask, onUpdateTask }: TaskItemProps) => {
 
   const handleStartEdit = () => setIsEditing(true);
   const handleCancelEdit = () => setIsEditing(false);
-  const handleSaveEdit = useCallback((newTitle: string) => {
+  const handleSaveEdit = (newTitle: string) => {
     const taskUpdated: Task = { id: task.id, title: newTitle };
     const success = onUpdateTask(taskUpdated);
     if (success) setIsEditing(false);
-  }, [task.id, onUpdateTask]);
+  };
 
   return (
     <li className="flex justify-between items-center gap-2 p-3 border rounded hober:bg-gray-50 transition-colors">
